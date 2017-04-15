a39a3344b5d948937dc51a686da68cfde7803202
Saulo Camposi <saulocamposi@gmail.com>
2017-04-15 22:48:04 +0100

    Add heroes component

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+  providers: [ HeroService ]
+})
+export class HeroesComponent implements OnInit {
+
+  title = 'Tour of Heroes';
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  constructor(private heroService: HeroService){}
+
+  ngOnInit(){
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+  }
+
+}
