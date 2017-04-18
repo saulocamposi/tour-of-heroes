5595fe2e392d0a8e0fdb0cee5b8a849adf8ee1ea
Saulo Camposi <saulocamposi@gmail.com>
2017-04-18 04:32:05 +0100

    Update delete method

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -54,7 +54,13 @@ export class HeroService {
       .catch(this.handleError);
   }
 
-  //add(){}
+  delete(id: number): Promise<void> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete(url, {headers: this.headers})
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
+  }
 
   private handleError(error: any): Promise<any>{
     console.log('An error occurred', error);
